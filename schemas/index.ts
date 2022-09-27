448808094443424ac363cc1d118ee55f59ba1578
Daniel <daniel@sanity.io>
2022-09-27 10:50:09 -0230

    Create a simple pet schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import pet from "./pet";
+
+export const schemaTypes = [pet]
---
+++ schemas/pet.ts
@@ -0,0 +1,42 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+    type: 'document',
+    name: 'pet',
+    title: 'Pet',
+    fields: [
+        defineField({
+            type: 'string',
+            name: 'name',
+            title: 'Name',
+            validation: (rule) => rule.required(),
+        }),
+        defineField({
+            type: 'string',
+            name: 'breed',
+            title: 'Breed',
+            options: {
+                list: ['Chihuahua']
+            }
+        }),
+        defineField({
+            type: 'string',
+            name: 'slogan',
+            title: 'Slogan',
+            validation: (rule) => rule.required()
+        }),
+        defineField({
+            type: 'text',
+            name: 'introduction',
+            title: 'Introduction',
+        }),
+        defineField({
+            type: 'array',
+            name: 'biography',
+            title: 'Biography',
+            of: [
+                {type: 'block'}
+            ]
+        })
+    ]
+})
